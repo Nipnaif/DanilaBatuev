1ac5e4b132c8c61c50400409424bdb04793f2a6e
Danila <danila.tasher07@mail.ru>
2024-12-15 13:14:39 +0500

    дз на 15.12

--- 2/current.py
+++ 2/current.py
@@ -1,1 +1,7 @@
-111
+print("x y z w")
+for x in range(0, 2):
+    for y in range(0, 2):
+        for z in range(0, 2):
+            for w in range(0, 2):
+                if not(y and (x or z) or not(y or z) or w):
+                    print(x, y, z, w)
--- 5/current.py
+++ 5/current.py
@@ -1,1 +1,9 @@
-111
+for n in range(1, 1000):
+    b = bin(n)[2:]
+    if n % 2 == 0:
+        b += bin(n)[-2:]
+    else:
+        b = '1' + b + '0'
+    r = int(b, 2)
+    if r < 100:
+        print(n)
